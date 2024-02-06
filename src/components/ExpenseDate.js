c2c8c2337ac959dab4a98056e0d46af0e29aa906
firas hamila <firas.hm.1996@gmail.com>
2024-02-06 15:50:08 +0100

    aq

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ExpenseDate = ({ date }) => {
+  const year = date.getFullYear();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
