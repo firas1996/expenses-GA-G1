1a8c5aa77643ce0abaaddbea198d68165394bccc
firas hamila <firas.hm.1996@gmail.com>
2024-02-06 14:45:57 +0100

    qq

--- src/components/ExpensesMain.js
+++ src/components/ExpensesMain.js
@@ -1,7 +1,12 @@
+import ExpenseItem from "./ExpenseItem";
 import "./ExpensesMain.css";
 
 const ExpensesMain = () => {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <ExpenseItem />
+    </div>
+  );
 };
 
 export default ExpensesMain;
