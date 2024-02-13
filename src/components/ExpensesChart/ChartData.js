4b56b43f4204fb81e8513be8c36bcbca2a68dbdd
firas hamila <firas.hm.1996@gmail.com>
2024-02-13 15:15:41 +0100

    nj

--- src/components/ExpensesChart/ChartData.js
+++ src/components/ExpensesChart/ChartData.js
@@ -16,6 +16,15 @@ const ChartData = ({ expenses }) => {
     { label: "Nov", value: 0 },
     { label: "Dec", value: 0 },
   ];
+  for (const item of expenses) {
+    data[item.date.getMonth()].value += item.price;
+  }
+  const values = data.map((item) => item.value);
+  const max = Math.max(...values);
+  const sum = values.reduce((a, b) => {
+    return a + b;
+  });
+  console.log(sum);
   return <Chart data={data} />;
 };
 
