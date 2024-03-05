ced0ed2815cd738e1f08db93f43ffa652bcfca1c
firas hamila <firas.hm.1996@gmail.com>
2024-03-05 14:48:19 +0100

    fghj

--- src/components/Form/AddExpenses.js
+++ src/components/Form/AddExpenses.js
@@ -1,26 +1,94 @@
+import { useState } from "react";
 import "./AddExpenses.css";
 
 const AddExpenses = () => {
+  // const [title, setTitle] = useState("");
+  // const [price, setPrice] = useState("");
+  // const [date, setDate] = useState("");
+  const [formInputs, setFormInputs] = useState({
+    title: "",
+    price: "",
+    date: "",
+  });
+  // const titleChangeHandler = (event) => {
+  //   // setTitle(event.target.value);
+  //   setFormInputs((prevState) => {
+  //     return { ...prevState, title: event.target.value };
+  //   });
+  // };
+  // const priceChangeHandler = (event) => {
+  //   // setPrice(event.target.value);
+  //   setFormInputs({ ...formInputs, price: event.target.value });
+  // };
+  // const dateChangeHandler = (event) => {
+  //   // setDate(event.target.value);
+  //   setFormInputs({ ...formInputs, date: event.target.value });
+  // };
+  const onChangeHandler = ({ target }) => {
+    const { name, value } = target;
+    setFormInputs((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(formInputs);
+    setFormInputs({
+      title: "",
+      price: "",
+      date: "",
+    });
+    // console.log(title);
+    // console.log(price);
+    // console.log(date);
+    // setTitle("");
+    // setPrice("");
+    // setDate("");
+  };
   return (
     <div className="new-expense">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input />
+            <input
+              required
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={formInputs.title}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Price</label>
-            <input />
+            <input
+              required
+              type="number"
+              placeholder="Price"
+              min="0"
+              step="0.25"
+              name="price"
+              value={formInputs.price}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input />
+            <input
+              required
+              type="date"
+              min="2021-01-01"
+              max="2025-12-31"
+              name="date"
+              value={formInputs.date}
+              onChange={onChangeHandler}
+            />
           </div>
         </div>
         <div className="new-expense__actions">
           <button>Cancel</button>
-          <button>Ajouter</button>
+          <button type="submit">Ajouter</button>
         </div>
       </form>
     </div>
